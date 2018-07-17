85cbfc9495721e5470b42e26def03dd95c373a4a
BaiRuobing <brb19960117@gmail.com>
2018-07-17 11:52:14 +0800

    7-17

--- miao/lodash/bairuobing-lodash.js
+++ miao/lodash/bairuobing-lodash.js
@@ -27,6 +27,15 @@ var bairuobing = {
         }
         return res
     },
+    concat: function(array, ...values) {
+        var val = values[0]
+        var res = array
+        for (var i = 1; i < values.length; i++) {
+            val = val.concat(values[i])
+        }
+        res = res.concat(val)
+        return res
+    },
     //即创建一个新数组,这个数组中的值,为第一个数字（array 参数）排除了给定数组中的值
     difference: function(array, ...values) {
         var res = []
@@ -49,5 +58,44 @@ var bairuobing = {
         }
         return res
     },
+    //
+    differenceBy: function(array, ...values) {
+
+    },
+    drop: function(array, number = 1) {
+        return array.slice(number,array.length)
+    },
+    //创建一个只接受一个参数的函数并返回,并且忽略掉其他参数
+    unary: function(func) {
+        return function(value){
+            return func(value)
+        }
+    },
+    //创建一个否定一个判定函数的函数,接受到的任何参数都给原函数,只不过返回的是原函数的相反函数罢了
+    negate: function(func) {
+        return function(...args) {
+            return !func(...args)
+        }
+    },
+    //forEach(collection, [iteratee=_.identity])
+    //collection (Array|Object): 一个用来迭代的集合
+    //[iteratee=_.identity] (Function): 每次迭代调用的函数
+    forEach: function(collection, func) {
+        if(Array.isArray(collection)) {
+            for(var i = 0; i < collection.length; i++) {
+                func(collection[i])
+            }
+            return collection
+        }
+        if(typeof collection === 'object') {
+            for(var item in collection) {
+                func(item,collection[item])
+            }
+            return collection
+        }
+    },
+
+
 
 }
+var __ = bairuobing
